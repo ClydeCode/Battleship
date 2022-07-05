4eb03f9268a1b1deaab1c9e2ae63ef54e839418c
Clyde <noneclyde@gmail.com>
2022-07-05 13:15:09 +0300

    Rewrite tests

--- src/_tests_/battleship.test.js
+++ src/_tests_/battleship.test.js
@@ -1,52 +1,34 @@
 import { Ship } from '../battleship';
 
-test('create ship with wrong length (1)', () => {
-  const ship = Ship(0);
-
-  expect(ship.getArray()).toBeUndefined();
-});
-
-test('create ship with wrong length (2)', () => {
-  const ship = Ship(7);
-
-  expect(ship.getArray()).toBeUndefined();
-});
-
-test('create ship with good length', () => {
-  const ship = Ship(2);
-
-  expect(ship.getArray()).toStrictEqual(['o', 'o']);
-});
-
 test('hit function (1) single hit', () => {
   const ship = Ship(2);
 
   ship.hit(1);
 
-  expect(ship.getArray()).toStrictEqual(['x', 'o']);
+  expect(ship.getHP()).toBe(1);
 });
 
 test('hit function (2) single hit', () => {
-  const ship = Ship(2);
+  const ship = Ship(5);
 
-  ship.hit(2);
+  ship.hit();
 
-  expect(ship.getArray()).toStrictEqual(['o', 'x']);
+  expect(ship.getHP()).toBe(4);
 });
 
 test('hit function (3) multiple hits', () => {
-  const ship = Ship(2);
+  const ship = Ship(4);
 
-  ship.hit(1);
-  ship.hit(2);
+  ship.hit();
+  ship.hit();
 
-  expect(ship.getArray()).toStrictEqual(['x', 'x']);
+  expect(ship.getHP()).toBe(2);
 });
 
 test('isSunk function (1)', () => {
   const ship = Ship(2);
 
-  ship.hit(1);
+  ship.hit();
 
   expect(ship.isSunk()).toBe(false);
 });
@@ -54,8 +36,8 @@ test('isSunk function (1)', () => {
 test('isSunk function (2)', () => {
   const ship = Ship(2);
 
-  ship.hit(1);
-  ship.hit(2);
+  ship.hit();
+  ship.hit();
 
   expect(ship.isSunk()).toBe(true);
 });
